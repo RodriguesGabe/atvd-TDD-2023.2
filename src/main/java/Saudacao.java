f883af4657f995294824d5d569fc4000620c5e6d
Gitgabe <cybergabe01@gmail.com>
2023-11-26 15:59:15 -0300

    add codigo e teste

--- src/main/java/Saudacao.java
+++ src/main/java/Saudacao.java
@@ -1,4 +1,28 @@
 public class Saudacao {
-    
-    
+
+    public String saudacao(String... names) {
+        if (names == null || names.length == 0 || names[0] == null) {
+            return "Olá, compre uma rifa minha";
+        }
+
+        if (names.length == 1) {
+            String name = names[0];
+            if (name.equals(name.toUpperCase())) {
+                return "OLÁ, " + name + "!";
+            }
+            return "Olá, " + name + "!";
+        }
+
+        StringBuilder builder = new StringBuilder("Olá");
+        for (int i = 0; i < names.length; i++) {
+            if (i == names.length - 1) {
+                builder.append(" e ");
+            } else if (i > 0) {
+                builder.append(", ");
+            }
+            builder.append(names[i]);
+        }
+        builder.append("!");
+        return builder.toString();
+    }
 }
--- src/test/java/SaudacaoTest.java
+++ src/test/java/SaudacaoTest.java
@@ -1,9 +1,53 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class SaudacaoTest {
+
     @Test
-    public void req01()
-    {
-        
+    public void req01() {
+        Saudacao saudacao = new Saudacao();
+        assertEquals("Olá, Joao!", saudacao.saudacao("Joao"));
+    }
+
+    @Test
+    public void req02() {
+        Saudacao saudacao = new Saudacao();
+        assertEquals("Olá, meu amigo!", saudacao.saudacao((String) null));
+    }
+
+    @Test
+    public void req03() {
+        Saudacao saudacao = new Saudacao();
+        assertEquals("OLÁ, JOSE!", saudacao.saudacao("JOSE"));
+    }
+
+    @Test
+    public void req04() {
+        Saudacao saudacao = new Saudacao();
+        assertEquals("Olá, Maria e Laura!", saudacao.saudacao("Maria", "Laura"));
+    }
+
+    @Test
+    public void req05() {
+        Saudacao saudacao = new Saudacao();
+        assertEquals("Olá, Ana, Bruno e Carlos!", saudacao.saudacao("Ana", "Bruno", "Carlos"));
+    }
+
+    @Test
+    public void req06() {
+        Saudacao saudacao = new Saudacao();
+        assertEquals("Olá, Ana, Carlos e OLÁ, BRUNO!", saudacao.saudacao("Ana", "BRUNO", "Carlos"));
+    }
+
+    @Test
+    public void req07() {
+        Saudacao saudacao = new Saudacao();
+        assertEquals("Olá, Joao, Marcos e Daiane!", saudacao.saudacao("Joao", "Marcos, Daiane"));
+    }
+
+    @Test
+    public void req08() {
+        Saudacao saudacao = new Saudacao();
+        assertEquals("Olá, Joao e Marcos, Daiane!", saudacao.saudacao("Joao", "\"Marcos, Daiane\""));
     }
 }
